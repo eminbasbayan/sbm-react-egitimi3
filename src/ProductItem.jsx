d9c2ad7b797881ab84d3d705727225c0d0c7ebc1
eminbasbayan <emin.basbayan@icloud.com>
2024-06-05 11:41:09 +0300

    component inline css

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,14 +1,33 @@
+const productItemCss = {
+  border: "1px solid #eee",
+  width: "250px",
+}
+
 function ProductItem() {
   return (
-    <div className="product-item">
+    <div
+      className="product-item"
+      style={productItemCss}
+    >
       <div className="product-image">
         <img
           src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
           alt="product title"
-          width={200}
+          style={{
+            width: "100%",
+            height: "250px",
+            objectFit: "cover"
+          }}
         />
       </div>
-      <div className="product-info">
+      <div
+        className="product-info"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "12px",
+        }}
+      >
         <strong>Title</strong>
         <span>200₺</span>
       </div>
@@ -16,4 +35,4 @@ function ProductItem() {
   );
 }
 
-export default ProductItem
+export default ProductItem;
