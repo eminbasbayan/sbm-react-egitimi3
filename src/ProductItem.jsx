1c171f348c190614a4bff03200b8599a3488be31
eminbasbayan <emin.basbayan@icloud.com>
2024-06-05 11:50:36 +0300

    css dosyasını component içinde kullanmak

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,33 +1,15 @@
-const productItemCss = {
-  border: "1px solid #eee",
-  width: "250px",
-}
+import "./ProductItem.css";
 
 function ProductItem() {
   return (
-    <div
-      className="product-item"
-      style={productItemCss}
-    >
+    <div className="product-item">
       <div className="product-image">
         <img
           src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
           alt="product title"
-          style={{
-            width: "100%",
-            height: "250px",
-            objectFit: "cover"
-          }}
         />
       </div>
-      <div
-        className="product-info"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "12px",
-        }}
-      >
+      <div className="product-info">
         <strong>Title</strong>
         <span>200₺</span>
       </div>
