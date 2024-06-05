d7780b673e13972e36a4715bb6c6a8eb26110ea2
eminbasbayan <emin.basbayan@icloud.com>
2024-06-05 14:00:16 +0300

    products dinamik prop aktarımı

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,22 +7,14 @@ function Products() {
     <div className="products-wrapper">
       <h2>Products</h2>
       <div className="products">
-        <ProductItem
-          image={productsData[0].image}
-          title={productsData[0].title}
-          price={productsData[0].price}
-        />
-        <ProductItem
-          image={productsData[1].image}
-          title={productsData[1].title}
-          price={productsData[1].price}
-        />
-        <ProductItem
-          image={productsData[2].image}
-          title={productsData[2].title}
-          price={productsData[2].price}
-        />
-       
+        {productsData.map((product) => (
+          <ProductItem
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
       </div>
     </div>
   );
