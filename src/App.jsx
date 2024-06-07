4673bc1fe85dd95b923966b0e3738efbf8db64cb
eminbasbayan <emin.basbayan@icloud.com>
2024-06-07 10:30:49 +0300

    ilk route oluşturma

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import Products from "./components/Products/Products";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
 import Header from "./components/Layout/Header";
+import HomePage from "./pages/HomePage";
+
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
   return (
     <div className="app container">
-      <Header  />
-      <br />
-      <Products />
+      <Header />
+
+      <RouterProvider router={router} />
+
       <ToastContainer />
     </div>
   );
